817403ecf195aed3d425d2d976712f04c809e1ba
sethclaw <sethclaw10@gmail.com>
2016-01-23 15:48:02 -0700

    starting lab2

---
+++ SethLawlorLab2/Token.h
@@ -0,0 +1,45 @@
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+
+using namespace std;
+
+enum TokenType {
+		COMMA,
+		PERIOD,
+		Q_MARK,
+		LEFT_PAREN,
+		RIGHT_PAREN,
+		COLON,
+		COLON_DASH,
+		SCHEMES,
+		FACTS,
+		RULES,
+		QUERIES,
+		ID,
+		STRING,
+		ERROR,
+		END
+	};
+
+class Token {
+	public:
+		Token();
+		~Token();
+		Token(TokenType, string, int);
+		
+		TokenType getType();
+		string getValue();
+		int getLine();
+		string getInfo();
+	private:
+		TokenType TType;
+		string TValue;
+		int LineNum;
+		string TokenTypeToString(TokenType tt);
+
+};
+
+#endif
